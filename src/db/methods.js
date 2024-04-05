d495d937f916e606e4bb6fc3dff3db76cddd8f4b
Oleg Sharikov <kekoleg@protonmail.com>
2024-04-05 22:48:30 +0800

    Refactor search lessons query

--- src/db/methods.js
+++ src/db/methods.js
@@ -12,11 +12,22 @@ export default function initDbMethods({ sequelize, models }) {
       studentsCountFrom,
       studentsCountTo,
     }) => {
-      // Filter lesson
       const lessonFilter = {
-        date_between: startDate && endDate && `"date" BETWEEN '${startDate}' AND '${endDate}'`,
-        date_start: !endDate && startDate && `"date" = '${startDate}'`,
-        status: status && `"status" = ${status}`,
+        date_between:
+          startDate && endDate && `"lessons"."date" BETWEEN '${startDate}' AND '${endDate}'`,
+        date_start: !endDate && startDate && `"lessons"."date" = '${startDate}'`,
+        status: status && `"lessons"."status" = ${status}`,
+        teacher_ids: teacherIds?.length > 0 && `"lesson_teachers"."teacher_id" IN (${teacherIds})`,
+      };
+      const studentsCountFilter = {
+        students_between:
+          studentsCountFrom &&
+          studentsCountTo &&
+          `GROUP BY "lessons"."id" HAVING COUNT(DISTINCT "lesson_students"."student_id") BETWEEN ${studentsCountFrom} AND ${studentsCountTo}`,
+        students_count:
+          !studentsCountTo &&
+          studentsCountFrom &&
+          `GROUP BY "lessons"."id" HAVING COUNT(DISTINCT "lesson_students"."student_id") = ${studentsCountFrom}`,
       };
 
       const lessonFilterOptionsList = Object.keys(lessonFilter).reduce(
@@ -32,84 +43,41 @@ export default function initDbMethods({ sequelize, models }) {
       const lessonFilterStringFinal =
         lessonFilterOptionsList.length > 0 ? `WHERE ${lessonFilterOptionsList.join(' AND ')}` : '';
 
-      // Filter joined data
-      const additionalFilter = {
-        teacher_ids: teacherIds?.length > 0 && `"teacher.id" IN (${teacherIds})`,
-        students_between:
-          studentsCountFrom &&
-          studentsCountTo &&
-          `"students_count" BETWEEN ${studentsCountFrom} AND ${studentsCountTo}`,
-        students_count:
-          !studentsCountTo && studentsCountFrom && `"students_count" = ${studentsCountFrom}`,
-      };
-
-      const additionalFilterOptionsList = Object.keys(additionalFilter).reduce(
-        (optionsList, currentOptionName) => {
-          if (additionalFilter[currentOptionName]) {
-            optionsList.push(additionalFilter[currentOptionName]);
-          }
-          return optionsList;
-        },
-        [],
-      );
-
-      const additionalFilterStringFinal =
-        additionalFilterOptionsList.length > 0
-          ? `WHERE ${additionalFilterOptionsList.join(' AND ')}`
-          : '';
-
       // TODO use knex XD
       return sequelize.query(
         `
-        WITH lessons_ids AS (
-          SELECT
-            "id"
-          FROM
-            "lessons"
-          ${lessonFilterStringFinal}
-          LIMIT ${limit} OFFSET ${offset}
-        )
-
-        SELECT
-          *
-        FROM (
-          -- Get students and teachers details
-          SELECT
+        -- Filter out lessons
+        WITH filtered_lessons AS (
+          SELECT DISTINCT
             "lessons"."id",
-            "lessons"."date",
-            "lessons"."title",
-            "lessons"."status",
-            "teachers"."id" AS "teacher.id",
-            "teachers"."name" AS "teacher.name",
-            "students"."id" AS "student.id",
-            "students"."name" AS "student.name",
-            "students->lesson_students"."visit" AS "student.visit"
+            "lessons"."date"
           FROM
             "lessons" AS "lessons"
-            LEFT JOIN ("lesson_teachers" AS "teachers->lesson_teachers"
-              LEFT JOIN "teachers" AS "teachers" ON "teachers"."id" = "teachers->lesson_teachers"."teacher_id") ON "lessons"."id" = "teachers->lesson_teachers"."lesson_id"
-            LEFT JOIN ("lesson_students" AS "students->lesson_students"
-              LEFT JOIN "students" AS "students" ON "students"."id" = "students->lesson_students"."student_id") ON "lessons"."id" = "students->lesson_students"."lesson_id"
-          WHERE
-            "lessons"."id" IN (SELECT "id" FROM lessons_ids))
-          AS lesson_students_teachers_details
-
-          -- Count students
-          JOIN (
-            SELECT
-              "lessons"."id", COUNT("students"."id") AS "students_count"
-            FROM
-              "lessons" AS "lessons"
-              LEFT JOIN ("lesson_students" AS "students->lesson_students"
-                LEFT JOIN "students" AS "students" ON "students"."id" = "students->lesson_students"."student_id") ON "lessons"."id" = "students->lesson_students"."lesson_id"
-            WHERE
-              "lessons"."id" IN (SELECT "id" FROM lessons_ids)
-            GROUP BY
-              "lessons"."id")
-          AS enrolled_students_count
-          ON lesson_students_teachers_details.id = enrolled_students_count.id
-        ${additionalFilterStringFinal}
-        ORDER BY "date" DESC;
+          LEFT JOIN "lesson_teachers" AS "lesson_teachers" ON "lessons"."id" = "lesson_teachers"."lesson_id"
+          LEFT JOIN "lesson_students" AS "lesson_students" ON "lessons"."id" = "lesson_students"."lesson_id"
+          ${lessonFilterStringFinal}
+          ${studentsCountFilter.students_between || studentsCountFilter.students_count || ''}
+          LIMIT ${limit} OFFSET ${offset}
+        )
+        -- Get students and teachers details
+        SELECT
+          "lessons"."id",
+          "lessons"."date",
+          "lessons"."title",
+          "lessons"."status",
+          "teachers"."id" AS "teacher.id",
+          "teachers"."name" AS "teacher.name",
+          "students"."id" AS "student.id",
+          "students"."name" AS "student.name",
+          "students->lesson_students"."visit" AS "student.visit"
+        FROM
+          "lessons" AS "lessons"
+        LEFT JOIN ("lesson_teachers" AS "teachers->lesson_teachers"
+          LEFT JOIN "teachers" AS "teachers" ON "teachers"."id" = "teachers->lesson_teachers"."teacher_id") ON "lessons"."id" = "teachers->lesson_teachers"."lesson_id"
+        LEFT JOIN ("lesson_students" AS "students->lesson_students"
+          LEFT JOIN "students" AS "students" ON "students"."id" = "students->lesson_students"."student_id") ON "lessons"."id" = "students->lesson_students"."lesson_id"
+        WHERE
+          "lessons"."id" IN (SELECT "id" FROM filtered_lessons);
         `,
         {
           nest: true,
